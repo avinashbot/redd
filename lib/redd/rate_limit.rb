d68d96e9013dcabaa5dacceeabd828884fb6eec7
Avinash Dwarapu <d.nash.avi@gmail.com>
2015-02-22 06:15:23 +0000

    Implement an awesome new RateLimit#burst! method.
    Also overhaul rate-limiting thanks to X-Ratelimit- headers. May be
    subject to tons of bugfixes.

--- lib/redd/rate_limit.rb
+++ lib/redd/rate_limit.rb
@@ -1,9 +1,6 @@
 # The main Redd module.
 module Redd
-  # The class that handles rate limiting for reddit. reddit does supply
-  # X-Ratelimit headers but using those headers instead would just lead to
-  # short bursts, so it's better to go at a constant speed and space out
-  # requests every second.
+  # The class that handles rate limiting for reddit.
   #
   # If you'd rather have short bursts or no rate limiting at all, it's easy to
   # write one yourself. A rate limiting class is any class that has an
@@ -11,40 +8,79 @@ module Redd
   # can also extract the headers from the response and use those instead. To
   # remove rate limiting entirely, follow the example below.
   #
-  # @example No Rate Limiting
-  #   class IWantToGetIPBanned
-  #     def after_limit
-  #       yield
-  #     end
-  #   end
-  #
-  #   client = Redd.it(..., rate_limit: IWantToGetIPBanned.new)
+  # @note To remove rate limiting entirely, just burst forever.
+  #     rt = Redd::RateLimit.new
+  #     rt.burst!(Float::INFINITY)
   class RateLimit
-    # @!attribute [r] last_request_time
-    # @return [Time] The time when the last request took place.
-    attr_reader :last_request_time
-
     # @!attribute [rw] gap
     # @return [Integer, Float] The minimum time between requests.
     attr_accessor :gap
 
+    # @!attribute [rw] burst_length
+    # @return [Integer] The number of requests left to burst.
+    attr_accessor :burst_length
+
+    # @!attribute [r] last_request
+    # @return [Time] The time the last request was made.
+    attr_reader :last_request
+
+    # @!attribute [r] used, remaining, reset
+    # @return [Integer] The data from reddit's response headers.
+    attr_reader :used, :remaining, :reset
+
     # @param [Float, Integer] gap The minimum time between each request.
     def initialize(gap)
       # Some time ages ago, because we never made a request.
-      @last_request_time = Time.at(0)
+      @last_request = Time.at(0)
       @gap = gap
+      @burst_length = 0
     end
 
-    # Sleep until 2 seconds have passed since the last request and perform the
-    # given request.
+    # Don't sleep for the next few requests.
+    # @param [Integer] times The number of times to ignore limiting.
+    # @return [Integer] The total times rate limiting will be ignored.
+    def burst!(times)
+      @burst_length += times
+    end
+
+    # Sleep until 1 second has passed since the last request and perform the
+    # given request unless bursting.
     #
-    # @yield A block.
-    # @return The return value of the block.
+    # @yield [Faraday::Response] A response.
+    # @return [Faraday::Response] The response.
     def after_limit
-      wait_time = @last_request_time + @gap - Time.now
-      sleep(wait_time) if wait_time > 0
-      @last_request_time = Time.now
-      yield
+      response = yield
+      update!(response)
+      sleep(wait_time)
+      response
+    end
+
+    private
+
+    # Update necessary info with each request.
+    # @param [Faraday::Response] response The response to the request made.
+    def update!(response)
+      @last_request = Time.now
+      %w(used remaining reset).each do |type|
+        value = response.headers["x-ratelimit-#{type}"]
+        instance_variable_set("@#{type}", value.to_i) unless value.nil?
+      end
+    end
+
+    # @return [Float, Integer] The number of seconds to sleep.
+    def wait_time
+      if @burst_length > 0 && @remaining > 0
+        # Don't sleep if we are in burst mode.
+        @burst_length -= 1
+        0
+      elsif @reset.nil? || @remaining.nil?
+        # Just guess if no headers were given (max 1 sec after last request).
+        time = @last_request_time - Time.now + @gap
+        time > 0 ? time : 0
+      else
+        # Most cases, spread out requests over available time.
+        @reset.to_f / (@gap * @remaining + 1)
+      end
     end
   end
 end
