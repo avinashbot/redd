9ed0881c89b20d19ed9783c07b767214eb260179
Andres Perez <andres.a.perezleon@gmail.com>
2016-04-30 00:14:06 -0700

    Return empty array when there are no children
    MoreComments can contain an empty MoreComments object that would though an error when trying to create the object. This avoids that issue

--- lib/redd/objects/more_comments.rb
+++ lib/redd/objects/more_comments.rb
@@ -3,7 +3,8 @@ module Redd
     # The model for a morecomments object
     class MoreComments < Array
       def initialize(_, attributes)
-        super(attributes[:children])
+        #Return an empty arrar if there are no children
+        super(attributes[:children]) if attributes[:children]
       end
     end
   end
