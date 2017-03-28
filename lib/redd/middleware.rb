7cc99dd7d4df5e37eaa0bec15b443876354b406c
Avinash Dwarapu <d.nash.avi@gmail.com>
2017-03-28 14:52:12 +0100

    Refactor Redd::Middleware.

--- lib/redd/middleware.rb
+++ lib/redd/middleware.rb
@@ -7,9 +7,6 @@ require_relative '../redd'
 
 module Redd
   # Rack middleware.
-  # TODO: verify state
-  # TODO: deal with error from reddit
-  # TODO: deal with access_denied
   # TODO: deal with setting the correct expiry time on the access
   class Middleware
     # @param opts [Hash] the options to create the object with
@@ -36,28 +33,42 @@ module Redd
     end
 
     def call(env)
-      dup.thread_unsafe_call(env)
+      # This is done for thread safety so that each thread has its own copy
+      # of the middleware logic.
+      dup._call(env)
     end
 
     protected
 
-    def thread_unsafe_call(env)
+    def _call(env)
       @request = Rack::Request.new(env)
-
-      # Redirect user to reddit for authentication.
       return redirect_to_reddit! if @request.path == @via
-      # Convert the code to an access token if returning from authentication.
-      create_session! if @request.base_url + @request.path == @redirect_uri
-      # Clear the state for any other request.
-      @request.session[:redd_state] = nil
-      # Load a Session model from the access token in the user's cookies.
-      @request.env['redd.session'] = parse_session
-      # Go through the rest of the app.
+
+      before_call
       @app.call(env)
+      after_call
     end
 
     private
 
+    # Do any setup before calling the rest of the application.
+    def before_call
+      # Convert the code to an access token if returning from authentication.
+      create_session! if @request.base_url + @request.path == @redirect_uri
+      # Clear the state for any other request.
+      @request.session.delete(:redd_state)
+      # Load a Session model from the access token in the user's cookies.
+      @request.env['redd.session'] = parse_session
+    end
+
+    # Do any cleanup or changes after calling the application.
+    def after_call
+      # Clear the session if the app explicitly set 'redd.session' to nil.
+      # XXX: does this seem like a good approach?
+      @request.session.delete(:redd_session) if @request.env['redd.session'].nil?
+    end
+
+    # Creates a unique state and redirects the user to reddit for authentication.
     def redirect_to_reddit!
       state = SecureRandom.urlsafe_base64
       url = Redd.url(
@@ -71,24 +82,31 @@ module Redd
       [302, { 'Location' => url, 'Content-Type' => 'text/html' }, ["<a href=\"#{url}\">Login</a>"]]
     end
 
+    # Assigns a single string representing a reddit authentication errors.
+    # TODO: would be preferable if we assigned an exception object instead...
     def handle_error
       return 'invalid_state' if @request.GET['state'] != @request.session[:redd_state]
       return @request.GET['error'] if @request.GET['error']
       nil
     end
 
+    # Store the access token and other details in the user's browser.
     def create_session!
+      # Skip authorizing if there was an error from the authorization.
       @request.env['redd.error'] = handle_error
       if @request.env['redd.error']
-        @request.session[:redd_session] = nil
+        @request.session.delete(:redd_session)
         return
       end
+
+      # Try to get a code (the rescue block will also prevent crazy crashes)
       access = @strategy.authenticate(@request.GET['code'])
       @request.session[:redd_session] = { token: access.to_h, authorized_at: Time.now.to_i }
     rescue Redd::ResponseError => e
       @request.env['redd.error'] = e
     end
 
+    # Return a {Redd::Models::Session} based on the hash saved into the browser's session.
     def parse_session
       return nil unless @request.session[:redd_session]
       client = Redd::APIClient.new(@strategy, user_agent: @user_agent, limit_time: 0)
